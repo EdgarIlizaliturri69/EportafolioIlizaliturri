c1955a90de63b45b099a6db5fb0af3202f4d4130
Edgar Ilizaliturri <edgar.ilizaliturri@uabc.edu.mx>
2023-11-25 16:56:21 -0800

    Agregar archivos de proyecto.

---
+++ EportafolioIlizaliturri/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EportafolioIlizaliturri.Startup))]
+namespace EportafolioIlizaliturri
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
